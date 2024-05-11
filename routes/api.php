384a51bd14d8edb355de9bedaaaedb80b98cb227
cy <cyrilschool12@gmail.com>
2024-05-11 15:25:11 +0800

    Initial import

---
+++ routes/api.php
@@ -0,0 +1,19 @@
+<?php
+
+use App\Http\Controllers\StudentController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+// Route::prefix('/students')->group(function () {
+//     Route::get('/', [StudentController::class, 'index']);
+//     Route::get('/{id}', [StudentController::class, 'select']);
+//     Route::post('/', [StudentController::class, 'create']);
+//     Route::patch('/{id}', [StudentController::class, 'update']);
+//     Route::delete('/{id}', [StudentController::class, 'delete']);
+// });
+
+Route::get('/students', [StudentController::class, 'index']);
+Route::get('/students/{id}', [StudentController::class, 'select']);
+Route::post('/students', [StudentController::class, 'create']);
+Route::patch('/students/{id}', [StudentController::class, 'update']);
+Route::delete('/students/{id}', [StudentController::class, 'delete']);
